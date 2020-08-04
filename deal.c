c853e611e417ae02bd0a1d05ec7147c8bb2e44c1
Jaden Ong <47911422+jadenong@users.noreply.github.com>
2020-08-04 20:17:40 +0800

    Update deal.c

--- deal.c
+++ deal.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
-#include <windows.h> # for windows
-#include <unistd.h> # for linux
+// #include <windows.h> // for windows
+// #include <unistd.h> // for linux
 
 int main(void)
 {
-	printf("For this trick, you'll need a deck of cards.");
+	int small_int;
+	printf("For this trick, you'll need a deck of cards.\n");
 	sleep(5);
-	printf("Shuffle the deck.");
+	printf("Shuffle the deck.\n");
 	sleep(5);
-	printf("Tell me a number between one and ten.");
+	printf("Tell me a number between one and ten.\n");
 	scanf("%d", &small_int);
 	sleep(5);
-	printf("Good. Now deal two deck with %d cards. One for you and one for me. Make sure both decks have the same number of cards.", small_int);
+	printf("Good. Now deal two deck with %d cards. One for you and one for me. Make sure both decks have the same number of cards.\n", small_int);
 	sleep(5);
-	printf("Pick up your pile and look at the bottom card of your deck. Memorize this. This is your card."
+	printf("Pick up your pile and look at the bottom card of your deck. Memorize this. This is your card.\n");
 	sleep(5);
-	printf("Put your pile on top of the deck that you've been dealing from. Now, every time I say 'Deal,' I want you to place a card, face-down, on the table, forming a new deck.");
-	sleep(5);
-	for(int i = 0; i < 11; i++)
-		{
-			printf("Deal.")
-			sleep(5);
-		}
-	printf("Stop.")
-	sleep(5);
-	printf("Now place your newly-formed deck on top of the deck you've been dealing with. Place also my deck on top of it. Start dealing again on my command."
+	printf("Put your pile on top of the deck that you've been dealing from. Now, every time I say 'Deal,' I want you to place a card, face-down, on the table, forming a new deck.\n");
 	sleep(5);
 	for(int i = 0; i < 11; i++)
 		{
-			printf("Deal.")
+			printf("Deal.\n");
 			sleep(5);
 		}
-	printf("Stop.")
+	printf("Stop.\n");
 	sleep(5);
-	printf("Turn over the top card of the deck. This is your card."
+	printf("Now place your newly-formed deck on top of the deck you've been dealing with. Place also my deck on top of it. Start dealing again on my command.\n");
+	sleep(5);
+	for(int i = 0; i < 11; i++)
+		{
+			printf("Deal.\n");
+			sleep(5);
+		}
+	printf("Stop.\n");
+	sleep(5);
+	printf("Turn over the top card of the deck. This is your card.\n");
 }
